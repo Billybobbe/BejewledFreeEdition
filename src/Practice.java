3523f85c95120f639fffb022fb14b136d8c7b34c
Pc <24dmontgomery@athenian.org>
2023-12-01 18:08:37 -0800

    I added textures.

--- src/Practice.java
+++ src/Practice.java
@@ -17,8 +17,8 @@ public class Practice {
 
     public static void main(String[] args) {
         glfwInit();
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
         window = glfwCreateWindow(1280, 720, "Test", NULL, NULL);
         glfwMakeContextCurrent(window);
         GL.createCapabilities();
@@ -36,7 +36,7 @@ public class Practice {
 
         BufferedImage ken;
         try {
-            ken = ImageIO.read(new File("C:/Users/David/Downloads/01-12-23_1329.jpg"));
+            ken = ImageIO.read(new File("./res/01-12-23_1329.jpg"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -46,7 +46,7 @@ public class Practice {
 
         for(int y = 0; y<ken.getHeight(); y++){
             for(int x = 0; x<ken.getWidth(); x++){
-                int pixel = ken.getRGB(x, y);
+                int pixel = pixels[y*ken.getWidth()+x];
                 buffer.put((byte) ((pixel >> 16) & 0xFF));
                 buffer.put((byte) ((pixel >> 8) & 0xFF)); //Credit to user Krythic for these values
                 buffer.put((byte) (pixel & 0xFF));
@@ -56,28 +56,28 @@ public class Practice {
         buffer.flip();
         int texture = glGenTextures();
         glBindTexture(GL_TEXTURE_2D, texture);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB4, ken.getWidth(), ken.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
 
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, ken.getWidth(), ken.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
         while(!userclosed.get()){
 
             glClear(GL_COLOR_BUFFER_BIT);
 
-            glBindTexture(texture, GL_TEXTURE_2D);
+            glBindTexture(GL_TEXTURE_2D, texture);
             glBegin(GL_QUADS);
 
-            glTexCoord2f(0,0);
+            glTexCoord2f(1,1);
             glVertex2d(-0.5,-0.5);
 
-            glTexCoord2f(1,0);
+            glTexCoord2f(0,1);
             glVertex2d(0.5,-0.5);
 
-            glTexCoord2f(1,1);
+            glTexCoord2f(0,0);
             glVertex2d(0.5, 0.5);
 
-            glTexCoord2f(0,1);
+            glTexCoord2f(1,0);
             glVertex2d(-0.5, 0.5);
 
             glEnd();
